c0acaebef8620d9062109292b13ad8951a7066cc
phuhk <phuhk@viettel.com.vn>
2022-05-05 11:15:53 +0700

    - Fix base code

--- src/main/java/com/viettel/vtskit/kafka/configuration/ConstantConfiguration.java
+++ src/main/java/com/viettel/vtskit/kafka/configuration/ConstantConfiguration.java
@@ -1,5 +1,5 @@
 package com.viettel.vtskit.kafka.configuration;
 
 public class ConstantConfiguration {
-    public static final String GREETING_MESSAGE = "Hello %s"; // 5MB
+    public static final String GREETING_MESSAGE = "Hello %s";
 }
